c8685236561a54f143c18078c03ba8374ea97749
Cyrus Harmon <ch-github@bobobeach.com>
2014-01-16 09:17:51 -0800

    remove scratch.lisp

--- scratch.lisp
+++
@@ -1,7 +0,0 @@
-
-
-(pushnew (hunchentoot-cgi::create-cgi-dispatcher-and-handler
-          "/cgi-bin/"
-          (merge-pathnames "cgi/" (ch-asdf:asdf-lookup-path "asdf:/hunchy")))
-         (hunchentoot-vhost::dispatch-table hunchy::*cyrusharmon-host*) :test #'equal)
-
